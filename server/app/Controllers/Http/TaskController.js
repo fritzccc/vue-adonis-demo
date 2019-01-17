462990c386f59201b997dfc6435283e9a76bf4d6
Fritz Chen <fredcfj@gmail.com>
2019-01-17 21:44:23 +0900

    add update & delete task

--- server/app/Controllers/Http/TaskController.js
+++ server/app/Controllers/Http/TaskController.js
@@ -1,16 +1,18 @@
 'use strict'
 const Post = use('App/Models/Post')
+const Task = use('App/Models/Task')
 const AuthService = use('App/Services/AuthService');
 
 class TaskController {
-  async index({auth,params}){
+  async index({auth, params}){
     const user = await auth.getUser();
     const { post_id } = params;
     const post = await Post.find(post_id);
     AuthService.verifyResource(post,user.id);
     return post.Tasks().fetch();
   }
-  async store({auth,request,params}){
+
+  async store({auth, request, params}){
     const user = await auth.getUser();
     const { post_id } = params;
     const post = await Post.find(post_id);
@@ -21,6 +23,28 @@ class TaskController {
       completed:false
     });
   }
+
+  async update({auth, request, params}){
+    const user = await auth.getUser();
+    const { id } = params;
+    const task = await Task.find(id);
+    const post = await task.Post().fetch();
+    AuthService.verifyResource(post,user.id);
+    task.merge(request.post());
+    await task.save();
+    return task
+  }
+
+  async destroy({auth, params}){
+    const user = await auth.getUser();
+    const { id } = params;
+    const task = await Task.find(id);
+    const post = await task.Post().fetch();
+    AuthService.verifyResource(post,user.id);
+    task.delete();
+    return task;
+  }
+
 }
 
 module.exports = TaskController
